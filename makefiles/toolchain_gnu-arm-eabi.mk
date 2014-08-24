565a415ac12df44a973d1a257aded9f8ecf50de7
Karoly Molnar <karoly@lyorak.com>
2014-08-24 12:26:17 +0200

    updated toolchain

--- makefiles/toolchain_gnu-arm-eabi.mk
+++ makefiles/toolchain_gnu-arm-eabi.mk
@@ -50,7 +50,7 @@ OBJDUMP			 = $(CROSSDEV)objdump
 
 # Check if the right version of the toolchain is available
 #
-CROSSDEV_VER_SUPPORTED	 = 4.7
+CROSSDEV_VER_SUPPORTED	 = 4.8
 CROSSDEV_VER_FOUND	 = $(shell $(CC) -dumpversion)
 
 ifeq (,$(findstring $(CROSSDEV_VER_SUPPORTED),$(CROSSDEV_VER_FOUND)))
